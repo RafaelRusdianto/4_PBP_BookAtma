2cbe8b98a7c74b8045df531d5f10acd9a68a8b36
RafaelRusdianto <230712325@students.uajy.ac.id>
2026-05-12 21:33:31 +0700

    api config emulator + web

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -1,3 +1,10 @@
+// For Android emulator, use
+
+// class ApiConfig {
+//   static const String baseUrl = 'http://10.0.2.2:8000/api';
+// }
+
+// For Web, use
 class ApiConfig {
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static const String baseUrl = 'http://127.0.0.1:8000/api';
 }
