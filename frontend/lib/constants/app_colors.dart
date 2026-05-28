e0a99bef0dd7ea3d9a1284d35d5a8687f97d6532
FilbertValent12299 <230712299@students.uajy.ac.id>
2026-05-28 14:48:16 +0700

    nambah warna doang blm masuk coding

---
+++ frontend/lib/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xFF0F172A);
+  static const Color primary = Color(0xFF0064D2);
+  static const Color accent = Color(0xFFFFD400);
+  static const Color white = Color(0xFFFAFAFA);
+  static const Color danger = Color(0xFFD32F2F);
+  static const Color textDisabled = Color(0xFF64748B);
+  static const Color borderInput = Color(0xFFCBD5E1);
+  static const Color placeholder = Color(0xFF94A3B8);
+  static const Color background = Color(0xFFE8EEF6);
+}
